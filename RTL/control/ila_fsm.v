0b047ab491dd314e5a2edc9b761e4dfda476eec0
Penghua Chen <chen756415756@outlook.com>
2025-03-09 08:24:07 +0800

    More code in ila_fsm

--- RTL/control/ila_fsm.v
+++ RTL/control/ila_fsm.v
@@ -6,6 +6,7 @@ module ila_fsm(
     input wire i_err_reporting,
     // assert when link re-initialization is detected
     input wire i_sync_request_tx,
+    input wire i_sync_de_assertion,
 
     // select which octect stream should be fed into 8b/10b encoder
     // 0: user data
@@ -15,11 +16,10 @@ module ila_fsm(
 );
 
 // states
-localparam [4:0] DETECT_ERR_REPORTING = 5'b00001;
-localparam [4:0] DETECT_DEASSERT = 5'b00010;
-localparam [4:0] CAL_ADJ = 5'b00100;
-localparam [4:0] SEND_K = 5'b01000;
-localparam [4:0] SEND_ILA = 5'b10000;
+localparam [3:0] DETECT_DEASSERT = 4'b0001;
+localparam [3:0] CAL_ADJ = 4'b0010;
+localparam [3:0] SEND_K = 4'b0100;
+localparam [3:0] SEND_ILA = 4'b1000;
 
 
 // FSM actions encode
@@ -27,8 +27,14 @@ localparam [2:0] SEND_USER_DATA = 3'd0;
 localparam [2:0] SEND_K = 3'd1;
 localparam [2:0] SEND_LANE_SEQ = 3'd2;
 
-reg [2:0] next_state;
-reg [2:0] current_state;
+reg [3:0] next_state;
+reg [3:0] current_state;
+
+reg sync_request_tx_d;
+reg err_reporting_d;
+
+reg phadj;
+reg phadj_valid;
 // number of frames the K sequence has been sent in
 // current link re-initialization procedure
 reg [3:0] k_frame_cnt;
@@ -38,15 +44,21 @@ reg [3:0] k_sequence_min_frame;
 // initial lane alignment procedure
 reg [8:0] ila_multiframe_cnt;
 
+always@(posedge clk) begin
+    sync_request_tx_d <= i_sync_request_tx;
+    err_reporting_d <= i_err_reporting;
+end
+
 always@(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      current_state <= DETECT_ERR_REPORTING;
+      current_state <= DETECT_DEASSERT;
       o_link_mux <= SEND_USER_DATA;
+      phadj <= 1'b0;
+      phadj_valid <= 1'b0;
     end else begin
         current_state <= next_state;
 
         case(current_state)
-            DETECT_ERR_REPORTING: o_link_mux <= SEND_USER_DATA;
             DETECT_DEASSERT: o_link_mux <= SEND_USER_DATA;
             CAL_ADJ: o_link_mux <= SEND_USER_DATA;
             SEND_K: o_link_mux <= SEND_K;
@@ -58,13 +70,31 @@ end
 
 always@(*) begin
     case(current_state)
-        DETECT_ERR_REPORTING: begin
+        DETECT_DEASSERT: begin
+            if (i_sync_de_assertion &&
+                err_reporting_d &&
+                !sync_request_tx_d) begin
+                next_state <= CAL_ADJ;
+            end else begin
+                next_state <= DETECT_DEASSERT;
+            end
         end
-        INIT_LANE: begin
+        CAL_ADJ: begin
+            if (!phadj && phadj_valid) begin
+            // in protocol the transition can only happen if phadj is 0 AND
+            // it's not a sync request, since this FSM only detects non sync
+            // request, aka error reporting, so we eliminated the sync request
+            // condition
+                next_state <= DETECT_DEASSERT;
+            end else if (phadj && phadj_valid) begin
+                next_state <= SEND_K;
+            end else begin
+                next_state <= CAL_ADJ;
+            end
         end
-        DATA_ENC: begin
+        SEND_K: begin
         end
-        default: next_state = SYNC;
+        default: next_state = DETECT_DEASSERT;
     endcase
 end
 
